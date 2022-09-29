0f16af3944ce0fa392b198e90cff5bf29dfe4565
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-29 18:49:47 +0530

    Trhee char condition applied

---
+++ regex/InvalidUserDetailsException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.regex;
+
+public class InvalidUserDetailsException extends Exception {
+	 public InvalidUserDetailsException(String message) {
+	        super(message);
+	    }
+}
--- regex/UserRegistrationRegex.java
+++ regex/UserRegistrationRegex.java
@@ -1,5 +1,19 @@
 package com.bridgelabz.regex;
 
-public class UserRegistrationRegex {
+import java.util.regex.Pattern;
 
+public class UserRegistrationRegex {
+	 public Validator name = (name) -> {
+	        Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{2,}");
+	        boolean isValid = pattern.matcher(name).matches();
+	        if (isValid) {
+	            return true;
+	        } else {
+	            throw new InvalidUserDetailsException("Invalid Name => " + name);
+	        }
+	    };
+}
+
+ interface Validator {
+    boolean validate(String data) throws InvalidUserDetailsException;
 }
