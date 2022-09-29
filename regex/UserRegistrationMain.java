04d7507757bee2e0267d6e3460aabafd9d18d8b9
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-29 18:52:06 +0530

    Trhee char condition applied on last name

--- regex/UserRegistrationMain.java
+++ regex/UserRegistrationMain.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 
 public class UserRegistrationMain {
 
-	public static void main(String[] args)  throws InvalidUserDetailsException {
-		
+	public static void main(String[] args) throws InvalidUserDetailsException {
+
 		UserRegistrationRegex regex = new UserRegistrationRegex();
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Enter first name: ");
+		regex.name.validate(scanner.next());
+
+		System.out.println("\nEnter last name: ");
 		regex.name.validate(scanner.next());
 	}
 
