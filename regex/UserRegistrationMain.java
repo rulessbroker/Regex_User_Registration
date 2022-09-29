c81141e81d8cee520c24a0eecd3452a2dcd69567
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-29 19:04:05 +0530

    validate rule 3

--- regex/UserRegistrationMain.java
+++ regex/UserRegistrationMain.java
@@ -14,16 +14,16 @@ public class UserRegistrationMain {
 
 		System.out.println("\nEnter last name: ");
 		regex.name.validate(scanner.next());
-		
+
 		System.out.println("\nEnter email: ");
-        regex.email.validate(scanner.next());
-        
-        System.out.println("\nEnter mobile no as per format: (e.g. 91 9867859848) ");
-        scanner.nextLine();
-        regex.mobileNo.validate(scanner.nextLine());
-        
-        System.out.println("\nEnter Password:");
-        regex.password.validate(scanner.next());
+		regex.email.validate(scanner.next());
+
+		System.out.println("\nEnter mobile no as per format: (e.g. 91 9867859848) ");
+		scanner.nextLine();
+		regex.mobileNo.validate(scanner.nextLine());
+
+		System.out.println("\nEnter Password:");
+		regex.password.validate(scanner.next());
 
 	}
 
