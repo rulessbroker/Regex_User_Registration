f7ea3291fed4e77d6c690ab15900eb36db5198f9
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-10-03 18:59:22 +0530

    Refactoring code with Throwing Exception

--- regex/InvalidUserDetailsException.java
+++ regex/InvalidUserDetailsException.java
@@ -2,6 +2,10 @@ package com.bridgelabz.regex;
 
 public class InvalidUserDetailsException extends Exception {
 	
+	public InvalidUserDetailsException() {
+		
+	}
+	
 	public InvalidUserDetailsException(String message) {
 		
 		super(message);
